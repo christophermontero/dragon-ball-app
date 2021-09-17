e904ec1f011015ca272d1efc22e4ed25d040e67b
Christopher <chris.to20@hotmail.com>
2021-09-17 12:40:14 -0500

    Component to search character by name is added

--- src/pages/SearchScreen.jsx
+++ src/pages/SearchScreen.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const SearchScreen = () => {
-  return <div>Search component</div>;
+  return (
+    <div className="container">
+      <h1>Search Screen</h1>
+      <hr />
+      <div className="row">
+        <div className="col-6">
+          <h2>Search</h2>
+          <form>
+            <label className="form-label w-100">
+              Character:{" "}
+              <input
+                placeholder="Insert the name..."
+                type="text"
+                className="form-control"
+                autoComplete="off"
+                value={""}
+              />
+            </label>
+            <button type="submit" className="btn btn-info w-100">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchScreen;
